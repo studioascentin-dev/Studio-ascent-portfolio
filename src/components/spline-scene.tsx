3b06e83be54455ec1f0acfc0db094481597c6ef1
Dev kumar Das <devd4149@gmail.com>
2025-07-28 07:35:15 +0000

    ok what about this <iframe src='https://my.spline.design/100followers-eY

--- src/components/spline-scene.tsx
+++ src/components/spline-scene.tsx
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline';
 export function SplineScene() {
   return (
     <div className="w-full h-full">
-      <Spline scene="https://prod.spline.design/6Wq1QhYQCMCC3I6e/scene.splinecode" />
+      <Spline scene="https://my.spline.design/100followers-eY7b4hu9JR2TUtdVOUl9gl3v/" />
     </div>
   );
 }

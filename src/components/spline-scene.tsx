ee6bd59a43a534b762b4bee32b74e546c6f58352
Dev kumar Das <devd4149@gmail.com>
2025-07-28 07:24:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/spline-scene.tsx
+++ src/components/spline-scene.tsx
@@ -4,6 +4,6 @@ import Spline from '@splinetool/react-spline';
 
 export function SplineScene() {
   return (
-    <Spline scene="https://app.spline.design/community/file/8cfb6748-f3dd-44dd-89fb-f46c7ab4186e" />
+    <Spline scene="https://prod.spline.design/6Wq1QhYQCMCC3I6e/scene.splinecode" />
   );
 }

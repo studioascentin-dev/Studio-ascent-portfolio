720fd7b7044ce44edc8d7c52f063d0f5e11544bc
Dev kumar Das <devd4149@gmail.com>
2025-10-11 15:13:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-ascent",
+  "appId": "1:388355438333:web:a89402862ecd3747b4b094",
+  "apiKey": "AIzaSyBThLFz83ZE6FMx-bKZ6_SpvT_UchnVMvE",
+  "authDomain": "studio-ascent.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "388355438333"
+};

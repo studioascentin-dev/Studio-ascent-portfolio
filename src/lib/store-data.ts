135e1394b9a3df4d0ae0dc11202ec1bce45f31fe
Dev kumar Das <devd4149@gmail.com>
2025-11-02 10:25:38 +0000

    i think you added the different razorpay link change it to the new one i

--- src/lib/store-data.ts
+++ src/lib/store-data.ts
@@ -58,7 +58,7 @@ export const storeItems = {
       originalPrice: 999,
       discount: '90% OFF',
       platform: 'Mac Only',
-      paymentLink: 'https://rzp.io/l/jerryflow',
+      paymentLink: 'pl_RaUv9x5EuHJY2V',
       installVideo: '/videos/installation of jerryflow.mp4',
        details: {
         fileSize: '15.8 MB',
@@ -385,5 +385,3 @@ export const storeItems = {
   ],
   applications: [],
 };
-
-    

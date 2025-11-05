6e59ace975246fc7b5db49b12e5e391f3328e107
Dev kumar Das <devd4149@gmail.com>
2025-11-05 13:52:35 +0000

    also the about in the navigation is not working properly it should take

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -51,7 +51,7 @@ export function AboutSection() {
     ]
 
     return (
-        <section className="py-24 md:py-32">
+        <section id="about" className="py-24 md:py-32">
             <motion.div 
                 className="container mx-auto px-4"
                 variants={sectionVariants}

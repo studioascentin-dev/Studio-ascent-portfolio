84751c7620f06eb68cff4a2c39803c3e1e6e00dc
Dev kumar Das <devd4149@gmail.com>
2025-11-05 14:50:35 +0000

    can you design these button as well across the website

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] hover:bg-primary/90 rounded-full",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
